8dd5b2f4cca6a198f8db9531b7d4797d9ce1bae3
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-01-07 12:39:14 +0530

    Update 05_iam.tf

--- terraform/05_iam.tf
+++ terraform/05_iam.tf
@@ -1,22 +1,22 @@
 resource "aws_iam_role" "ecs-host-role" {
   name               = "ecs_host_role_prod"
-  assume_role_policy = file("/Users/praveensingampalli/Documents/BOOTCAMP2_FINAL/Terraform_withcontainer/terraform/policies/ecs-role.json")
+  assume_role_policy = file("/Users/praveensingampalli/Documents/BOOTCAMP2_FINAL/Terraform_withcontainers/terraform/policies/ecs-role.json")
 }
 
 resource "aws_iam_role_policy" "ecs-instance-role-policy" {
   name   = "ecs_instance_role_policy"
-  policy = file("/Users/praveensingampalli/Documents/BOOTCAMP2_FINAL/Terraform_withcontainer/terraform/policies/ecs-instance-role-policy.json")
+  policy = file("/Users/praveensingampalli/Documents/BOOTCAMP2_FINAL/Terraform_withcontainers/terraform/policies/ecs-instance-role-policy.json")
   role   = aws_iam_role.ecs-host-role.id
 }
 
 resource "aws_iam_role" "ecs-service-role" {
   name               = "ecs_service_role_prod"
-  assume_role_policy = file("/Users/praveensingampalli/Documents/BOOTCAMP2_FINAL/Terraform_withcontainer/terraform/policies/ecs-role.json")
+  assume_role_policy = file("/Users/praveensingampalli/Documents/BOOTCAMP2_FINAL/Terraform_withcontainers/terraform/policies/ecs-role.json")
 }
 
 resource "aws_iam_role_policy" "ecs-service-role-policy" {
   name   = "ecs_service_role_policy"
-  policy = file("/Users/praveensingampalli/Documents/BOOTCAMP2_FINAL/Terraform_withcontainer/terraform/policies/ecs-service-role-policy.json")
+  policy = file("/Users/praveensingampalli/Documents/BOOTCAMP2_FINAL/Terraform_withcontainers/terraform/policies/ecs-service-role-policy.json")
   role   = aws_iam_role.ecs-service-role.id
 }
 
